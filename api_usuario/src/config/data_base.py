5674de24b8c049f84dc0e7f46101004834de1e10
lucas-moia <pessoalucas95@gmail.com>
2025-09-12 01:24:18 -0300

    implementação do banco

--- api_usuario/src/config/data_base.py
+++ api_usuario/src/config/data_base.py
@@ -4,9 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    # Lê a URL de conexão do banco de dados das variáveis de ambiente.
-    # O valor padrão é para um banco SQLite, útil para testes locais sem Docker.
-    database_url = os.getenv('DATABASE_URL', 'sqlite:///market_management.db')
+    
+    database_url = os.getenv('DATABASE_URL', "mysql+pymysql://root:barbearia123@localhost:3306/barbearia_db")
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
